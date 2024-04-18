005e5816937802d3a6b2159708968cc3853bf3a1
Lukas Bronski <109726913+webski-dev@users.noreply.github.com>
2024-04-19 01:43:01 +0200

    Add files via upload

--- js/sliders.js
+++ js/sliders.js
@@ -1,19 +1,19 @@
-const slider = document.getElementById("myRange");
-const output = document.getElementById("investAmmount");
+const sliderAmmount = document.getElementById("RangeAmmount");
+const ammountOutput = document.getElementById("investAmmount");
 
-const sliderProportion = document.getElementById("myRangeProportion");
-const output2 = document.getElementById("investProportion");
+const sliderProportion = document.getElementById("RangeProportion");
+const goldProportion = document.getElementById("investProportionGold");
 
-const output3 = document.getElementById("investProportionSilver");
+const silverProportion = document.getElementById("investProportionSilver");
 
-output.innerHTML = slider.value;
-output2.innerHTML = sliderProportion.value;
+ammountOutput.innerHTML = sliderAmmount.value;
+goldProportion.innerHTML = sliderProportion.value;
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
+sliderAmmount.oninput = function () {
+  ammountOutput.innerHTML = this.value;
 };
 
 sliderProportion.oninput = function () {
-  output2.innerHTML = this.value;
-  output3.innerHTML = 100 - this.value;
+  goldProportion.innerHTML = this.value;
+  silverProportion.innerHTML = 100 - this.value;
 };
